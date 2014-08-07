ca8664ddc68b2f8205c5a86bdf81626e506daddb
Yura Vlasiuk <yuriivlasiuk@gmail.com>
2014-08-07 19:32:40 +0300

    Tasks form unit 9 done!

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,11 +28,7 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_title(user.name) }
       it { should have_link('Users',       href: users_path) }
@@ -53,6 +49,12 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "page should not have links" do
+        before { visit root_path }
+          it { should_not have_link('Profile',     href: user_path(user)) }
+          it { should_not have_link('Settings',    href: edit_user_path(user)) }
+      end
+
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -65,6 +67,19 @@ describe "Authentication" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
